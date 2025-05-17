aa9e4a92cff26bfb6d5bf9000dfde9c235f996df
typingspeedacademy <typingspeedaccadamy@gmail.com>
2025-05-17 22:54:24 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const app = express();
 const PORT = process.env.PORT || 10000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
